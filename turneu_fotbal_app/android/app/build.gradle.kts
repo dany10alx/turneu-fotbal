4150233010f285bb0599bd77f939a7c1800cea32
dany10alx <298386864+dany10alx@users.noreply.github.com>
2026-09-26 18:31:36 +0300

    app release

--- turneu_fotbal_app/android/app/build.gradle.kts
+++ turneu_fotbal_app/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.example.turneu_fotbal_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
